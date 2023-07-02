3b9fd53c49980e23eeb544d8b4c3de3abdd8dd75
Taeyoung Son <taeyoungson10@gmail.com>
2023-07-02 15:30:43 +0900

    feat: Project.create 함수 구현

--- Tuist/ProjectDescriptionHelpers/Project+Templates.swift
+++ Tuist/ProjectDescriptionHelpers/Project+Templates.swift
@@ -6,71 +6,104 @@ import ProjectDescription
 /// See https://docs.tuist.io/guides/helpers/
 
 extension Project {
-    /// Helper function to create the Project for this ExampleApp
-    public static func app(name: String, platform: Platform, additionalTargets: [String]) -> Project {
-        var targets = makeAppTargets(name: name,
-                                     platform: platform,
-                                     dependencies: additionalTargets.map { TargetDependency.target(name: $0) })
-        targets += additionalTargets.flatMap({ makeFrameworkTargets(name: $0, platform: platform) })
-        return Project(name: name,
-                       organizationName: "tuist.io",
-                       targets: targets)
-    }
-
-    // MARK: - Private
-
-    /// Helper function to create a framework target and an associated unit test target
-    private static func makeFrameworkTargets(name: String, platform: Platform) -> [Target] {
-        let sources = Target(name: name,
-                platform: platform,
-                product: .framework,
-                bundleId: "io.tuist.\(name)",
-                infoPlist: .default,
-                sources: ["Targets/\(name)/Sources/**"],
-                resources: [],
-                dependencies: [])
-        let tests = Target(name: "\(name)Tests",
-                platform: platform,
-                product: .unitTests,
-                bundleId: "io.tuist.\(name)Tests",
-                infoPlist: .default,
-                sources: ["Targets/\(name)/Tests/**"],
-                resources: [],
-                dependencies: [.target(name: name)])
-        return [sources, tests]
-    }
-
-    /// Helper function to create the application target and the unit test target.
-    private static func makeAppTargets(name: String, platform: Platform, dependencies: [TargetDependency]) -> [Target] {
-        let platform: Platform = platform
+    public static func create(name: String,
+                              products: [Product],
+                              dependencies: [TargetDependency],
+                              includeDemoApp: Bool = false) -> Project {
+        let platform = Platform.iOS
         let infoPlist: [String: InfoPlist.Value] = [
             "CFBundleShortVersionString": "1.0",
             "CFBundleVersion": "1",
             "UIMainStoryboardFile": "",
             "UILaunchStoryboardName": "LaunchScreen"
             ]
-
-        let mainTarget = Target(
-            name: name,
-            platform: platform,
-            product: .app,
-            bundleId: "io.tuist.\(name)",
-            infoPlist: .extendingDefault(with: infoPlist),
-            sources: ["Targets/\(name)/Sources/**"],
-            resources: ["Targets/\(name)/Resources/**"],
-            dependencies: dependencies
-        )
-
-        let testTarget = Target(
-            name: "\(name)Tests",
-            platform: platform,
-            product: .unitTests,
-            bundleId: "io.tuist.\(name)Tests",
-            infoPlist: .default,
-            sources: ["Targets/\(name)/Tests/**"],
-            dependencies: [
-                .target(name: "\(name)")
-        ])
-        return [mainTarget, testTarget]
+        var targets = [Target]()
+        var schemes = [Scheme]()
+        
+        products.forEach {
+            var target: Target
+            switch $0 {
+            case .app:
+                target = Target(
+                    name: name,
+                    platform: platform,
+                    product: .app,
+                    bundleId: "io.tuist.\(name)",
+                    infoPlist: .extendingDefault(with: infoPlist),
+                    sources: ["Sources/**"],
+                    resources: ["Resources/**"],
+                    dependencies: dependencies
+                )
+            case .unitTests:
+                target = Target(
+                    name: "\(name)Tests",
+                    platform: platform,
+                    product: .unitTests,
+                    bundleId: "io.tuist.\(name)Tests",
+                    infoPlist: .default,
+                    sources: ["Tests/**"],
+                    dependencies: [
+                        .target(name: "\(name)")
+                ])
+            case .uiTests:
+                target = Target(
+                    name: "\(name)UITests",
+                    platform: platform,
+                    product: .unitTests,
+                    bundleId: "io.tuist.\(name)UITests",
+                    infoPlist: .default,
+                    sources: ["UITests/**"],
+                    dependencies: [
+                        .target(name: "\(name)")
+                ])
+            case .framework, .staticFramework:
+                target = Target(
+                    name: name,
+                    platform: platform,
+                    product: $0,
+                    bundleId: "io.tuist.\(name)",
+                    infoPlist: .extendingDefault(with: infoPlist),
+                    sources: ["Sources/**"],
+                    resources: $0 == .framework ? ["Targets/\(name)/Resources/**"] : nil,
+                    dependencies: dependencies
+                )
+            case .dynamicLibrary, .staticLibrary:
+                target = Target(
+                    name: name,
+                    platform: platform,
+                    product: $0,
+                    bundleId: "io.tuist.\(name)",
+                    infoPlist: .extendingDefault(with: infoPlist),
+                    sources: ["Sources/**"],
+                    resources: nil,
+                    dependencies: dependencies
+                )
+            default: return
+            }
+            targets.append(target)
+        }
+        
+        if includeDemoApp {
+            let target = Target(
+                name: "\(name)DemoApp",
+                platform: platform,
+                product: .app,
+                bundleId: "io.tuist.\(name)DemoApp",
+                infoPlist: .extendingDefault(with: infoPlist),
+                sources: ["Sources/**"],
+                resources: ["Resources/**"],
+                dependencies: dependencies
+            )
+            targets.append(target)
+            
+            let scheme = Scheme(
+              name: "\(name)DemoApp",
+              buildAction: .init(targets: ["\(name)DemoApp"]),
+              runAction: .runAction(executable: "\(name)DemoApp")
+            )
+            schemes.append(scheme)
+        }
+        
+        return Project(name: name, targets: targets, schemes: schemes)
     }
 }
