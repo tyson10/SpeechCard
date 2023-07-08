7c1a3e2fe0797b2e057b55702161f7b60336e40e
Taeyoung Son <taeyoungson10@gmail.com>
2023-07-08 12:05:08 +0900

    build: CommonUI 추가

--- Workspace.swift
+++ Workspace.swift
@@ -11,6 +11,7 @@ let workspace = Workspace(
     name: "SpeechCard",
     projects: [
         "SpeechCardApp/**",
+        "CommonUI/**",
         "Core/Extensions/**",
         "Core/Model/**"
     ]
