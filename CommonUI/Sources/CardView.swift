9b5f37c815d63cfa47c5868467fab09b6f1c4ae2
Taeyoung Son <taeyoungson10@gmail.com>
2023-07-08 12:32:23 +0900

    feat: CardView 추가

---
+++ CommonUI/Sources/CardView.swift
@@ -0,0 +1,20 @@
+//
+//  CardView.swift
+//  CommonUI
+//
+//  Created by Taeyoung Son on 2023/07/08.
+//
+
+import SwiftUI
+
+public struct CardView: View {
+    public var body: some View {
+        Text("hello")
+    }
+}
+
+struct RepositoriesView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView()
+    }
+}
