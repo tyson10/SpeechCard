612988a804c695ed67a79a76e56a0ab5c4e1b157
Taeyoung Son <taeyoungson10@gmail.com>
2024-04-29 23:43:25 +0900

    feat: 앱 및 뷰 생성

---
+++ Feature/Challenge/Sources/ChallengeApp.swift
@@ -0,0 +1,19 @@
+//
+//  ChallengeApp.swift
+//  Challenge
+//
+//  Created by Taeyoung Son on 4/29/24.
+//
+
+import SwiftUI
+
+import Domain
+
+@main
+struct ShelfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ChallengeView()
+        }
+    }
+}
