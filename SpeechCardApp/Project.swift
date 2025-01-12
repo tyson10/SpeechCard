ff2011a39cb9245f87e7b5f08680bf0668f1a734
Taeyoung Son <taeyoungson10@gmail.com>
2025-01-12 16:03:17 +0900

    tuist: SpeechCardApp 모듈에 음성인식 권한 description 정의

--- SpeechCardApp/Project.swift
+++ SpeechCardApp/Project.swift
@@ -10,5 +10,9 @@ let project = Project.create(
     ],
     dependencies: [
         .Project.DIContainer.container
+    ],
+    additionalInfo: [
+        "NSMicrophoneUsageDescription": "우리 앱에서 음성 인식을 위해 마이크에 접근해야 합니다.",
+        "NSSpeechRecognitionUsageDescription": "우리 앱에서 음성 인식을 위해 음성 입력을 사용합니다."
     ]
 )
