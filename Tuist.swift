002a304c886241fc81a60b0a82d4a733f21dbe65
Taeyoung Son <taeyoungson10@gmail.com>
2025-12-08 12:54:03 +0900

    tuist: v4.111.1 버전 업데이트

--- Tuist.swift
+++ Tuist.swift
@@ -2,6 +2,6 @@ import ProjectDescription
 
 let config = Config(
     plugins: [
-        .local(path: .relativeToManifest("../../Plugins/SpeechCard")),
+        .local(path: .relativeToManifest("../Plugins/SpeechCard")),
     ]
 )
